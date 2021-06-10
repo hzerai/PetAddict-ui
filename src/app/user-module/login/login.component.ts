4d160defd78e7621649bfba4fda8282e55bff71a
Habib Zerai <hpiplolz@gmail.com>
2021-06-10 15:17:33 +0100

    register page

--- src/app/user-module/login/login.component.ts
+++ src/app/user-module/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit {
   errorMessage = '';
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService,private _location: Location) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -29,19 +30,20 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const {email, password } = this.form;
+    const { email, password } = this.form;
 
     this.authService.login(email, password).subscribe(
       data => {
         this.tokenStorage.saveToken(data.token);
         this.tokenStorage.saveUser(data);
-
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.tokenStorage.getUser().roles;
-        this._location.back();
+        this.router.navigate(['/']);
+
       },
       err => {
+        console.log(err.error)
         this.errorMessage = err.error.message;
         this.isLoginFailed = true;
       }
