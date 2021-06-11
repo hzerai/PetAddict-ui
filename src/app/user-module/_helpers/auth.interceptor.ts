8e6780c8fcd71ea726dcec2fbb6fb26c387c312c
yassin <yassin.zouari@outlook.com>
2021-06-11 02:34:41 +0100

    security

--- src/app/user-module/_helpers/auth.interceptor.ts
+++ src/app/user-module/_helpers/auth.interceptor.ts
@@ -15,21 +15,22 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
-    if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-    }
-    return next.handle(authReq).pipe(catchError(x=> this.handleAuthError(x))); //here use an arrow function, otherwise you may get "Cannot read property 'navigate' of undefined" on angular 4.4.2/net core 2/webpack 2.70
+      const token = this.token.getToken();
+      if (token != null) {
+        authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      }
+      return next.handle(authReq).pipe(catchError(x=> this.handleAuthError(x))); //here use an arrow function, otherwise you may get "Cannot read property 'navigate' of undefined" on angular 4.4.2/net core 2/webpack 2.70
   }
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     //handle your auth error or rethrow
-    if (err.status === 401 || err.status === 403) {
+    if (this.router.url!="/login" && (err.status === 401 || err.status === 403)) {
         //navigate /delete cookies or whatever
         this.router.navigateByUrl(`/login`);
         // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
         return of(err.message); // or EMPTY may be appropriate here
     }
+
     return throwError(err);
 }
 }
