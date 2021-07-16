f9cb002a39fac5d0f6378fd47b553d990e7d1fe9
Habib Zerai <hpiplolz@gmail.com>
2021-07-16 23:05:48 +0100

    fix image for veto admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backUrl: "https://73205f2fcb01.ngrok.io/api",
+  backUrl: "https://a860b631edc9.ngrok.io/api",
   rabbitMq : 'ws://localhost:15674/ws/',
   rabbitMqUser : 'guest',
   rabbitMqPassword : 'guest'
